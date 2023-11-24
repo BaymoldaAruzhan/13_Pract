f45cb18c93e740a4fff24cd75d4d63078490d47e
Aru2003 <baymolda03@inbox.ru>
2023-11-24 17:41:59 +0600

    Добавьте файлы проекта.

---
+++ 06November_13Pract/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _06November_13Pract
+{
+    public class Program
+    {
+        static void Exmpl01()//коллекция
+        {
+            ArrayList list = new ArrayList();
+            object test = 55;//boxing
+            int x = (int)test;//unboxing
+
+            list.Add(5);//boxing
+            list.Add(true);
+            list.Add("Test");
+
+            foreach (int item in list)
+            {
+                int xx = (int)item;
+            }
+        }
+        //queue(очередь)
+        static void Exmpl02()
+        {
+            Queue qe = new Queue();
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                qe.Enqueue(i);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(qe.Dequeue());
+            }
+            Console.WriteLine("Очередь\n");
+            foreach (var item in qe)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Exmpl03()
+        {
+            Stack stack = new Stack();
+            stack.Push("1");
+            stack.Push("2");
+            stack.Push("3");
+            stack.Push("4");
+
+            Console.WriteLine("Очередь\n");
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Exmpl04()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            for (int i = 0; i < 10; i++)
+            {
+                dic.Add(1, testc.ToString());
+            }
+        }
+
+        static void Exmpl05()
+        {
+
+        }
+
+    }
+}
---
+++ Exmpl05/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exmpl05
+{
+    class MusicCatalog
+    {
+        private Hashtable catalog = new Hashtable();
+
+        public void AddDisk(string diskName)
+        {
+            if (!catalog.ContainsKey(diskName))
+            {
+                catalog[diskName] = new List<string>();
+            }
+        }
+
+        public void RemoveDisk(string diskName)
+        {
+            if (catalog.ContainsKey(diskName))
+            {
+                catalog.Remove(diskName);
+            }
+        }
+
+        public void AddSong(string diskName, string artist, string songTitle)
+        {
+            if (catalog.ContainsKey(diskName))
+            {
+                var songs = (List<string>)catalog[diskName];
+                songs.Add($"{artist} - {songTitle}");
+            }
+        }
+
+        public void RemoveSong(string diskName, string artist, string songTitle)
+        {
+            if (catalog.ContainsKey(diskName))
+            {
+                var songs = (List<string>)catalog[diskName];
+                songs.Remove($"{artist} - {songTitle}");
+            }
+        }
+
+        public void ShowCatalog()
+        {
+            Console.WriteLine("Каталог музыкальных компакт-дисков:");
+
+            foreach (DictionaryEntry entry in catalog)
+            {
+                Console.WriteLine($"Диск: {entry.Key}");
+
+                var songs = (List<string>)entry.Value;
+                foreach (var song in songs)
+                {
+                    Console.WriteLine($"  - {song}");
+                }
+            }
+        }
+
+        public void ShowSongsByArtist(string artist)
+        {
+            Console.WriteLine($"Поиск записей исполнителя {artist}:");
+
+            foreach (DictionaryEntry entry in catalog)
+            {
+                var songs = (List<string>)entry.Value;
+                var artistSongs = songs.Where(s => s.StartsWith($"{artist} - "));
+
+                foreach (var song in artistSongs)
+                {
+                    Console.WriteLine($"  - {song}");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MusicCatalog catalog = new MusicCatalog();
+
+            catalog.AddDisk("Disk 1");
+            catalog.AddSong("Disk 1", "Artist 1", "Song 1");
+            catalog.AddSong("Disk 1", "Artist 2", "Song 2");
+
+            catalog.AddDisk("Disk 2");
+            catalog.AddSong("Disk 2", "Artist 1", "Song 3");
+
+            catalog.ShowCatalog();
+
+            catalog.RemoveDisk("Disk 1");
+
+            catalog.ShowCatalog();
+
+            catalog.ShowSongsByArtist("Artist 1");
+        }
+    }
+}
